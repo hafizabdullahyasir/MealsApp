d2bee57191bdabdb93d53340897a1946b5a77cf8
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 12:56:32 +0500

    use on press in categorygriditle through props

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,9 @@
 import { View, Text, Pressable, StyleSheet, Platform} from "react-native";
 
-export default function CategoryGridTile({title, color}){
+export default function CategoryGridTile({title, color, onPress}){
     return(
        <View style={styles.gridItem }>
-                    <Pressable android_ripple={{color: '#ccc'}} style={({pressed})=> [styles.button, pressed ? styles.buttonPressed : null]}>
+                    <Pressable android_ripple={{color: '#ccc'}} style={({pressed})=> [styles.button, pressed ? styles.buttonPressed : null]} onPress={onPress}>
             <View style={[styles.innerContainer, {backgroundColor: color}]}>
                 <Text style={styles.title}>{title}</Text>
             </View>
