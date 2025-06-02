e09189da84865a5fb909357a67c4abd72e41bbeb
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 17:05:06 +0500

    fetch category from categories and then uselayouteffect hook to handle rendring smooth with animation

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,13 +1,29 @@
 import { View, Text, StyleSheet, FlatList } from "react-native";  
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
 
-export default function MealsOverview({ route }) {
+
+export default function MealsOverview({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItemData) => {
     return mealItemData.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(()=>{
+    const categoryTitle = CATEGORIES.find((category)=> category.id === catId).title;
+
+
+    navigation.setOptions({
+      title: categoryTitle
+    });
+
+
+
+  },[catId, navigation])
+
+
 
   function renderMealItem(itemData) {
 
