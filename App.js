c4dc32946bc7d6676056f00aaa17d088926f3d8b
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 11:50:43 +0500

    make the status bar to dark from light

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
-    <StatusBar style="light" />
-      <CategoriesScreen />
+    <StatusBar style="dark" />
+    <NavigationContainer style={styles.container} > 
+      <Stack.Navigator>
+        <Stack.Screen name="MealsCategories"  component={CategoriesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
       </>
  
   );
@@ -13,6 +22,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-   
+    flex: 1,
   },
 });
