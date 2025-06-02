bcfc61b07417c2f090a3b99c1898dd91e9e680bb
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 15:19:39 +0500

    added image in the MealItem component

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text , Pressable, Image, StyleSheet   } from "react-native";
 
 
-export default function MealItem({title}){
+export default function MealItem({title, imageUrl}){
     return (
         <View >
 
-           <Text>{title}</Text>
+          <Pressable>
+          <View>
+         <Image source={{uri: imageUrl} } style={styles.image} />
+         <Text style={styles.title}>{title}</Text>
+
+
+
+
+          </View>
+
+
+
+          </Pressable>
 
             </View>
             
@@ -18,16 +30,16 @@ export default function MealItem({title}){
     }
 
 
-// const styles = StyleSheet.create({
-//     mealItem: {
-//         margin: 16,
-//         borderRadius: 8,
-//         overflow: 'hidden',
-//         backgroundColor: 'white',
-//         elevation: 4,
-//         shadowColor: 'black',
-//         shadowOpacity: 0.25,
-//         shadowOffset: {width: 0, height: 2},
-//         shadowRadius: 8,
-//     },
-// });
+const styles = StyleSheet.create({
+    image:{
+        width: '100%',
+        height: 200,
+    },
+    title:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8,
+    },
+   
+});
