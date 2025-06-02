74a1bbd623e9f8f0e11d76108ce337563e894922
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 16:04:39 +0500

    Mealitem pressable styling

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,12 @@
-import { View, Text , Pressable, Image, StyleSheet   } from "react-native";
+import { View, Text , Pressable, Image, StyleSheet, Platform   } from "react-native";
 
 
 export default function MealItem({title, imageUrl, duration, complexity, affordability}){
     return (
-        <View >
+        <View style={styles.mealItem}>
 
-          <Pressable>
+          <Pressable android_ripple={{color: '#ccc'}} style={({pressed}) => (pressed ? styles.buttonPressed : null)}>
+            <View style={styles.innerContainer}>
           <View>
          <Image source={{uri: imageUrl} } style={styles.image} />
          <Text style={styles.title}>{title}</Text>
@@ -15,12 +16,12 @@ export default function MealItem({title, imageUrl, duration, complexity, afforda
 
           </View>
 
-       <View>
-        <Text>{duration}</Text>
-        <Text>{complexity}</Text>
-        <Text>{affordability}</Text>
+       <View style={styles.details}>
+        <Text style={styles.detailItem}>{duration}</Text>
+        <Text style={styles.detailItem}>{complexity}</Text>
+        <Text style={styles.detailItem}>{affordability}</Text>
        </View>
-
+       </View>
           </Pressable>
 
             </View>
@@ -35,6 +36,24 @@ export default function MealItem({title, imageUrl, duration, complexity, afforda
 
 
 const styles = StyleSheet.create({
+
+ mealItem:{
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.35,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 16,      
+ }, 
+ innerContainer:{
+    borderRadius:8,
+    overflow: 'hidden',
+},
+
+
     image:{
         width: '100%',
         height: 200,
@@ -45,5 +64,19 @@ const styles = StyleSheet.create({
         fontSize: 18,
         margin: 8,
     },
+    details:{
+        flexDirection: 'row',
+        alignItems: 'center',
+       justifyContent: 'center',
+        padding: 8,
+      
+    },
+    detailItem:{
+        marginHorizontal: 4,
+        fontSize: 12,
+    },
+    buttonPressed:{
+        opacity: 0.5,
+    }
    
 });
