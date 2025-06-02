3eec71b002c16fb66d051f75b6cd8b94cde01dbf
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-02 13:36:35 +0500

    added display meals function

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -6,6 +6,12 @@ export default function MealsOverview({route}) {
 const catId = route.params.categoryId;
 
 
+
+const displayedMeals = MEALS.filter((mealItemData)=>{
+    return mealItemData.categoryIds.indexOf(catId) >= 0;
+})
+
+
     return (
         <View style={styles.container}>
                 <Text>Meals Overview - {catId}</Text>
