5e3d9e97c59a28e3369e6b3b27359553b0194182
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-06-11 15:41:10 +0500

    updatd favorite screen to show favorite meals on

--- screens/Favourite.js
+++ screens/Favourite.js
@@ -1,9 +1,39 @@
-import { Text } from "react-native" 
+import { useContext } from "react"
+import { Text, View, StyleSheet } from "react-native";
+import MealsList from "../components/MealsList/MealsList"
+import { FavouritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 
 export default function Favourite(){
+const favoriteMealsCtx = useContext(FavouritesContext);
 
-return <Text>The favourites Screen !</Text>
+const favoriteMeals = MEALS.filter((meal)=> favoriteMealsCtx.ids.includes(meal.id))
+
+if(favoriteMeals.length === 0){
+    return (
+        <View style={styles.rootContainer}>
+            <Text style={styles.text}>You have no favorite meals yet</Text>
+        </View>
+    )
+}
+
+
+return <MealsList items={favoriteMeals}/>
 
 
 }
+
+const styles = StyleSheet.create({
+    rootContainer : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    
+    },
+    text:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black'
+    }
+});
